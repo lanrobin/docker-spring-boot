19cfdec60dc845d0a5e445e99ca4ccd9f89a2d2d
Robin Lan <rolan@microsoft.com>
2021-04-25 17:33:57 +0800

    调试RBAC完成了。

--- src/main/java/com/example/demo/pojo/rbac/RBACUser.java
+++ src/main/java/com/example/demo/pojo/rbac/RBACUser.java
@@ -40,12 +40,12 @@ public class RBACUser implements UserDetails {
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 
     public void setUsername(String username) {
