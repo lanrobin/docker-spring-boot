773398946f9d817441e56f31f7a242df4ca739d3
Robin Lan <rolan@microsoft.com>
2021-04-26 12:39:01 +0800

    Add index for rbac_user's username to improve performance.

--- src/main/resources/sql/rabc-schema.sql
+++ src/main/resources/sql/rabc-schema.sql
@@ -43,7 +43,8 @@ CREATE TABLE `rbac_user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `locked` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  INDEX index_username (username)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 
